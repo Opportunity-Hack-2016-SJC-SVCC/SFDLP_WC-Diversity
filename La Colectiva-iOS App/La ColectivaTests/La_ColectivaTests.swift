17ed0adba65e423268cb49372edcb957a5a96a7b
nvohra <nvohra@paypal.com>
2016-09-24 16:09:02 -0700

    adding the preliminary storyboard designs

---
+++ La Colectiva-iOS App/La ColectivaTests/La_ColectivaTests.swift
@@ -0,0 +1,36 @@
+//
+//  La_ColectivaTests.swift
+//  La ColectivaTests
+//
+//  Created by Vohra, Nikant on 9/24/16.
+//  Copyright © 2016 Vohra, Nikant. All rights reserved.
+//
+
+import XCTest
+@testable import La_Colectiva
+
+class La_ColectivaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

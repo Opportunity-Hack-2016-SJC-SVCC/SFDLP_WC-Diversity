e622689e23d0eeba2eb37d4de943b4b966bb4518
Ubuntu <hack@Ubuntu.1jjevylxjljujjpomt4bxlogqe.dx.internal.cloudapp.net>
2016-09-25 02:32:41 +0000

    Inital fix to bugs

--- WebApp/app/python/databaseSetup.py
+++ WebApp/app/python/databaseSetup.py
@@ -18,7 +18,7 @@ class Worker(Base):
     workerEnglishSkill = Column(String(100), nullable = False)
     workerGender = Column(String(10), nullable = False)
     workerUserName = Column(String(80), nullable = False)
-    workerStartDate = Column(Date, nullable = False) 
+    workerStartDate = Column(String(100), nullable = False) 
     workerPicUrl = Column(String(1000), nullable = False)     
 
 class Skill(Base):
@@ -31,28 +31,35 @@ class Event(Base):
     __tablename__ = 'event'
     eventID = Column(Integer, primary_key=True)
     eventName = Column(String(100), nullable = False)
-    eventStartDate = Column(DateTime, nullable = False)
-    eventEndDate = Column(DateTime, nullable = False)
+    eventStartDate = Column(String(100), nullable = False)
+    eventEndDate = Column(String(100), nullable = False)
     eventDetails = Column(String(1000), nullable = False)
     eventPicUrl = Column(String(1000), nullable = False) 
     eventCategory = Column(String(1), nullable = False) 
     
 class WorkerSkill(Base):
     __tablename__ = 'workerskill'
-    WorkerID = Column(Integer, ForeignKey(worker.workerID))
-    SkillID = Column(Integer, ForeignKey(skill.skillID))
+    worker = relationship(Worker)
+    skill = relationship(Skill)
+    ROW_ID = Column(Integer, primary_key = True)
+    WorkerID = Column(Integer, ForeignKey('worker.workerID'))
+    SkillID = Column(Integer, ForeignKey('skill.skillID'))
 
 class EventHistory(Base):
     __tablename__ = 'eventhistory'
+    worker = relationship(Worker)
+    event = relationship(Event)
     RowID = Column(Integer, primary_key = True)
-    WorkerID = Column(Integer, ForeignKey(worker.workerID))
-    EventID = Column(Integer, ForeignKey(event.eventID))
-    date = Column(Date, nullable = False)
+    WorkerID = Column(Integer, ForeignKey('worker.workerID'))
+    EventID = Column(Integer, ForeignKey('event.eventID'))
+    date = Column(String(80), nullable = False)
 
 class CheckIn(Base):
     __tablename__ = 'checkin'
-    WorkerID = Column(Integer, ForeignKey(worker.workerID))
-    date = Column(Date, nullable = False) 
+    worker = relationship(Worker)
+    ROWID = Column(Integer, primary_key = True)
+    WorkerID = Column(Integer, ForeignKey('worker.workerID'))
+    date = Column(String(80), nullable = False) 
 
 #This line has to be at the end of the file
 engine = create_engine('sqlite:///info.db')

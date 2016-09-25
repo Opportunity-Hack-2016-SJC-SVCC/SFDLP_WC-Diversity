4f3ce926d7649cf90218dacf01a5f4d4528c5bf6
nvohra <nvohra@paypal.com>
2016-09-24 22:10:37 -0700

    signup view5 completed

---
+++ La Colectiva-iOS App/La Colectiva/SignupViewController5.swift
@@ -0,0 +1,49 @@
+//
+//  SignupViewController5.swift
+//  La Colectiva
+//
+//  Created by Vohra, Nikant on 9/24/16.
+//  Copyright © 2016 Vohra, Nikant. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController5: UIViewController {
+
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true;
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        indicator.startAnimating()
+        
+        // Runs after 1 second on the main queue.
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2) ) {
+            self.indicator.stopAnimating()
+            self.performSegue(withIdentifier: "HomeSegue", sender: self)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
